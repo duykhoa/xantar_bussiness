576e30a50e889cd9a147c4eb26fec315e627fff9
duykhoa <duykhoa12t@gmail.com>
2014-02-27 23:04:33 +0700

    Using amazon service for searching locality by zipcode

--- app/models/bussiness.rb
+++ app/models/bussiness.rb
@@ -1,7 +1,9 @@
 class Bussiness < ActiveRecord::Base
+  require 'open-uri'
+  require 'json'
 
   def self.zip_code_or_place_name places_params
-    places_params.to_i > 0 ? ZIP_CODE.find(places_params)['city'] : places_params rescue {"$blank" => false}
+    places_params.to_i > 0 ? find_city_by_zipcode(places_params) : places_params rescue {"$blank" => false}
   end
 
   def self.fatual_results query, params
@@ -15,5 +17,9 @@ class Bussiness < ActiveRecord::Base
     category_rows = query_by_category.rows if query_by_category
 
     [category_rows.to_a + query_by_place_name.rows].uniq { |item| item['factual_id'] }
+  end
+
+  def self.find_city_by_zipcode zipcode
+    JSON.parse(open("https://s3.amazonaws.com/zips.dryan.io/#{zipcode}.json").read)["locality"]
   end
 end
